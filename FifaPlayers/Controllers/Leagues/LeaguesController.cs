c8f16b67da530e965ceca38c6cb2848f56c5d876
Thomas Roberts <thom.and.roberts@gmail.com>
2018-12-03 22:15:49 +0000

    Added leagues controller.

---
+++ FifaPlayers/Controllers/Leagues/LeaguesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FifaPlayers.DAOs.Leagues;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FifaPlayers.Controllers.Leagues
+{
+    [Route("league")]
+    public class LeaguesController : Controller
+    {
+        private LeagueDAO leagueDAO;
+        public LeaguesController(LeagueDAO leagueDAO)
+        {
+            this.leagueDAO = leagueDAO;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
