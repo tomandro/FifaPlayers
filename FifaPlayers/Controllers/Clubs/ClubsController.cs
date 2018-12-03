448635e87e260f89754e69dd1ac5ca2a2f780d52
Thomas Roberts <thom.and.roberts@gmail.com>
2018-12-03 22:24:12 +0000

    Created clubs controller

---
+++ FifaPlayers/Controllers/Clubs/ClubsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FifaPlayers.Controllers.Clubs
+{
+    [Route("Clubs")]
+    public class ClubsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
