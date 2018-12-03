c1fd0082eff03284a2b16772614bf739186135f6
Thomas Roberts <thom.and.roberts@gmail.com>
2018-12-03 22:25:47 +0000

    Added a dependency injection so a ClubDAO is passed to the clubs controller

--- FifaPlayers/Controllers/Clubs/ClubsController.cs
+++ FifaPlayers/Controllers/Clubs/ClubsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FifaPlayers.DAOs.Clubs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FifaPlayers.Controllers.Clubs
@@ -9,6 +10,12 @@ namespace FifaPlayers.Controllers.Clubs
     [Route("Clubs")]
     public class ClubsController : Controller
     {
+        private ClubDAO clubDAO;
+        public ClubsController(ClubDAO clubDAO)
+        {
+            this.clubDAO = clubDAO;
+        }
+
         public IActionResult Index()
         {
             return View();
